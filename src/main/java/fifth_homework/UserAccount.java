531ec62c55d65a23104640cecfdb7e953d489b69
Maxde1 <maxdeniss56@gmail.com>
2021-09-14 12:01:15 +0300

    Doing 5 homework

---
+++ src/main/java/fifth_homework/UserAccount.java
@@ -0,0 +1,27 @@
+package fifth_homework;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.*;
+import javax.validation.constraints.*;
+import javax.xml.crypto.Data;
+import java.util.Set;
+import lombok;
+@lombok.Data
+public class UserAccount {
+    @NotNull(message = "This field can't be null")
+    @Size(min = 2, max = 26, message = "Name can't be less that 2 symbol or bigger than 26 symbol")
+    private String name;
+    //Phone number format that valid for Ukraine
+    @Pattern(regexp = "380\\d{7}\\b", message = "Number format should be 380*******, where * is digit")
+    private String phoneNumber;
+    @Pattern(regexp = "male|female", message = "Invalid value, value can be \"male\" or \"female\"")
+    private String sex;
+    @Past(message = "Invalid value, birthday can't be future date")
+    private Data birthday;
+    @Email
+    private String email;
+    private
+
+
+}
---
+++ src/test/java/fifth_homework/TestBeenValidator.java
@@ -0,0 +1,24 @@
+package fifth_homework;
+
+import org.testng.annotations.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+public class TestBeenValidator {
+    @Test
+    public void positiveTestValidationUserAccount(){
+        UserAccount userAccount = new UserAccount();
+        userAccount.setName("M");
+        userAccount.setPhoneNumber("34324");
+        userAccount.setSex("female");
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        Validator validator = validatorFactory.getValidator();
+        Set<ConstraintViolation<UserAccount>> constraintViolations = validator.validate(userAccount);
+
+        validatorFactory.close();
+    }
+}
